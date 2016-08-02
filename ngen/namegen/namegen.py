91718742993fd2e3ca6377c1056f5891b22fc273
Theo Beffart <tbeffart@gmail.com>
2016-08-02 18:04:09 +0000

    update sql query, determine letter after db access

--- ngen/namegen/namegen.py
+++ ngen/namegen/namegen.py
@@ -3,19 +3,19 @@ import sqlite3 as lite
 import sys, random
 import string
 import re
-def gen(catAlist, catNlist, letter):
+def gen(catAlist, catNlist, letters):
     con = lite.connect('namegen/namegen.db')
 
-    cmdnoun="select noun from nouns join categories on category=id where name in ("+ '?,'*(len(catNlist)-1) +"?)  and char=?"
-    cmdadj="select adjective from adjectives join adjcategories on category=id where name in ("+ '?,'*(len(catAlist)-1) +"?) and char=?"
+    cmdnoun="select noun from nouns join categories on category=id where name in ("+ '?,'*(len(catNlist)-1) +"?)  and char in(" + '?,'*(len(letters)-1)+"?)"
+    cmdadj="select adjective from adjectives join adjcategories on category=id where name in ("+ '?,'*(len(catAlist)-1) +"?) and char in(" + '?,'*(len(letters)-1)+"?)"
 
     with con:
         cur = con.cursor()
-        cur.execute(cmdnoun,(catNlist+[letter]))
-        rowsN = cur.fetchall()
-        cur.execute(cmdadj,(catAlist+[letter]))
-        rowsA = cur.fetchall()
-        if len(rowsN) and len(rowsA):
-            return random.choice(rowsA)[0]+"<br>"+random.choice(rowsN)[0]
+        cmd = "select * from (("+cmdadj+") , ("+cmdnoun+")) where SUBSTR(noun,1,1)=SUBSTR(adjective,1,1)"
+	cur.execute(cmd,(catAlist+letters+catNlist+letters))
+        rows = cur.fetchall()
+        if len(rows) :
+		name = random.choice(rows)
+        	return name[0]+"<br>"+name[1]
         else:
-            return "Impossible<br>Input"
+		return "Impossible<br>Input"
