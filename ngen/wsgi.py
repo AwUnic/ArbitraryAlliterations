43b754c5c769a96581727485886ee9aa72ce2736
theo <tbeffart@gmail.com>
2016-08-02 10:21:12 +0200

    add support for separators in input array

--- ngen/wsgi.py
+++ ngen/wsgi.py
@@ -2,15 +2,17 @@
 import sqlite3 as lite
 import sys, random
 import string
-from namegen import namegen 
+from namegen import namegen
 from cgi import parse_qs, escape
 
 def  application(env, start_response):
 	parameters = parse_qs(env.get('QUERY_STRING', ''))
-	start_response('200 OK', [('Content-Type', 'text/html')])	
-	categories = parameters['types[]']	
+	start_response('200 OK', [('Content-Type', 'text/html')])
+	categories = parameters['types[]']
+	brkC = categories.index(':catbrk:')
+	brkL = categories.length+1
 	letter = random.choice(string.ascii_uppercase)
-	catA = ['feelings']
-	catN = categories	
+	catA = categories[brkC+1:brkL]
+	catN = categories[:brkC]	
 	resp = '<p>'+namegen.gen(catA,catN,letter)+'</p>\n'
 	return [resp.encode('utf8')]
